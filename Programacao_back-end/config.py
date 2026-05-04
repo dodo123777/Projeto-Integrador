cef45e67782c65ccc7712e600c4641f1d90f92d7
diuliano <diuliano@romi.com>
2026-05-04 14:37:06 -0300

    fix (config.py) - Remoção de fallback de variaveis sensiveis

--- Programacao_back-end/config.py
+++ Programacao_back-end/config.py
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "SEVEN_2024_SUPERSECRETO")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_PORT = os.getenv("DB_PORT")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     GEMINI_MODEL = os.getenv("GEMINI_MODEL")
